f5571910aa49563e0773a25b04529ff2f342cfa5
Abolfazl Ghorbani <ab.ghorbani28@gmail.com>
2022-07-01 09:57:32 +0200

    Create-Basic-Example-Dark mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const getMode = () => {
+    const initialMode = localStorage.getItem("mode");
+    if (initialMode == null) {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return true
+      } else {
+        return false;
+      }
+    } else {
+      return JSON.parse(localStorage.getItem("mode"));
+    }
+  }
+
+  const [dark, setDark] = useState(getMode());
+  
+  useEffect(()=>{
+    localStorage.setItem("mode", JSON.stringify(dark))
+  }, [dark])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={dark ? "app dark-mode" : "app"}>
+      <header className="header">
+        <div className="container">
+          <div className="nav">
+            <ul>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Contact Us  </li>
+            </ul>
+            <label className="switch">
+              <input 
+               type="checkbox"
+               onChange={()=> setDark(!dark)}
+               checked={dark}
+              />
+              <span className="slider round"></span>
+            </label>
+          </div>
+        </div>
       </header>
+      <div className="content">
+          <div className="container">
+            <h1>{dark ? "Dark mode is activated and we have a dark space" : "Light mode is activated and the space is bright"}</h1>
+            <p>This is light mode and now the screen is white</p>
+          </div>
+          <div className="container">
+            <div className="card">
+              <div className="card-item">
+                <img src="image/1.jpg" alt="" />
+                <h2>Top Angebot</h2>
+                <p>Produkte von Philips</p>
+              </div>
+              <div className="card-item">
+                <img src="image/2.jpg" alt="" />
+                <h2>Top Angebot</h2>
+                <p>Produkte von Philips</p>
+              </div>
+              <div className="card-item">
+                <img src="image/3.jpg" alt="" />
+                <h2>Top Angebot</h2>
+                <p>Produkte von Philips</p>
+              </div>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
